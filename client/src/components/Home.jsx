dcdc9b05a80ea2758a86396df1f236f505365399
Hyun Bin Yim <hby0818@gmail.com>
2023-02-21 18:52:23 -0500

    updates

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,27 @@
+import axios from 'axios'
 import RideList from './RideList'
+import { useEffect, useState } from "react"
+const BASE_URL = `http://localhost:3001`
+
 
 const Home = () => {
+  const [rides, setRides] = useState([])
+
+  useEffect(() => {
+    const getRides = async () => {
+      const response = await axios.get(`${BASE_URL}/getAllRides`)
+      console.log(response)
+    }
+    getRides()
+  }, [])
+
+
   return (
-    <div>
+    <div className='rides' key={rides._id}>
       <h1>Rides</h1>
-      <RideList />
+      {rides.map((ride) => (
+      <RideList name={ride.name} image={ride.image} location={ride.location} />
+      ))}
     </div>
   )
 }
