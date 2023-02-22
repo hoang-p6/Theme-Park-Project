ebdda70d0c2ef8938df37d129c71a93681dbb5f7
Michael Durando <mjdurando82@yahoo.com>
2023-02-21 19:36:54 -0500

    post method created

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import './App.css'
 import Nav from './components/Nav'
 import Home from './components/Home'
 import { Routes, Route } from 'react-router-dom'
+import AddRide from './components/AddRide'
 
 const App = () => {
   return (
     <div>
       <Nav />
       <main>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createRide" element={<AddRide />} />
+        </Routes>
       </main>
     </div>
   )
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,37 @@
+import axios from 'axios'
+import AddRide from './AddRide'
 import RideList from './RideList'
+import { useEffect, useState } from "react"
+
+const BASE_URL = `http://localhost:3001/api`
+
 const Home = () => {
+  const [rides, setRides] = useState([])
+  const [addingRide, setAddingRide] = useState(false)
+
+  const getRides = async () => {
+    const response = await axios.get(`${BASE_URL}/getAllRides`)
+    setRides(response.data.rides)
+  }
+
+  useEffect(() => {
+    getRides()
+  }, [])
+
+  const addRide = () => {
+    setAddingRide(true)
+  }
+
   return (
-    <div>
+    <div className='rides' key={rides._id}>
       <h1>Rides</h1>
-      <RideList />
+      {rides.map((ride) => (
+      <RideList name={ride.name} image={ride.image} location={ride.location} />
+      ))}
+      <button onClick={addRide}>Add Ride</button>
+      {addingRide && (
+        <AddRide />
+      )}
     </div>
   )
 }
