a44e69cb54fb50dff1106dcfba69df5491d2308e
Phong Hoang <p.hoang6900@gmail.com>
2023-02-21 23:42:32 -0500

    Updated add review and added update ride component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,29 +1,25 @@
 import axios from 'axios'
-
 import AddRide from './AddRide'
 import RideList from './RideList'
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const BASE_URL = `http://localhost:3001/api`
 
 const Home = () => {
   const [rides, setRides] = useState([])
-
+  const { id } = useParams()
   const [addingRide, setAddingRide] = useState(false)
 
   const getRides = async () => {
     const response = await axios.get(`${BASE_URL}/getAllRides`)
     setRides(response.data.rides)
-    console.log(response)
   }
-
   useEffect(() => {
-
     const getRides = async () => {
       const response = await axios.get(`${BASE_URL}/getAllRides`)
       setRides(response.data.rides)
     }
-
 
     getRides()
   }, [])
@@ -33,24 +29,22 @@ const Home = () => {
   }
 
   return (
-    <div className="rides" key={rides._id}>
-      <h1>Rides</h1>
-      {rides.map((ride) => (
-        <RideList
-          name={ride.name}
-          image={ride.image}
-          location={ride.location}
-          id={ride._id}
-          getRides={getRides}
-        />
-      ))}
-      <button onClick={addRide}>Add Ride</button>
-
-      {addingRide && (
-        <AddRide getRides={getRides}/>
-        
-      )}
-
+    <div>
+      <div className="rides" key={rides._id}>
+        {rides.map((ride) => (
+          <RideList
+            name={ride.name}
+            image={ride.image}
+            location={ride.location}
+            id={ride._id}
+            getRides={getRides}
+          />
+        ))}
+      </div>
+      <button onClick={addRide} className="addRideButton">
+        Add Ride
+      </button>
+      {addingRide && <AddRide getRides={getRides} />}
     </div>
   )
 }
---
+++ client/src/components/UpdateRide.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const UpdateRide = ({ rides }) => {
+  return <div></div>
+}
+
+export default UpdateRide
