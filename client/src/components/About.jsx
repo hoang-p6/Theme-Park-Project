31ace49a73cb5cd3a94c657e2f00a0134848de0e
Michael Durando <mjdurando82@yahoo.com>
2023-02-22 21:23:46 -0500

    about page

---
+++ client/src/components/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h2>About</h2>
+      <big>If you're an adrenaline junkie like us, you've come to the right place. Navigate through this page to see the best roller coasters around the world. Click on them to find your favorite!</big>
+        <div>
+        <img className='about-img'src='https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY4NjM4OTY4NjkyNzQ2MTM1/best-amusement-parks-in-the-world.jpg' alt='theme-park'></img>
+        </div>
+  </div>
+  )
+}
+
+export default About
